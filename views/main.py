0d91e18c72b06f5e8ddc60329dc99a8024b696a9
Mariana Vasconcelos <mavapisi16@icloud.com>
2025-06-07 02:03:32 +0100

    Main interface and buttons

--- views/main.py
+++ views/main.py
@@ -35,3 +35,55 @@ class MainWindow(ctk.CTk):
             height = 35,
         )
         start_button.pack(pady=20)
+
+        line_separator = ctk.CTkLabel(
+            self,
+            text = '',
+            height = 1,
+            width = 300,
+            fg_color = "#cccccc"
+        )
+        line_separator.pack(pady=(0, 10))
+
+        button_style = {
+            "font": ctk.CTkFont("Calibri", size=17),
+            "fg_color": "#4d7c85",
+            "hover_color": "#3d8491",
+            "width": 120,
+            "height": 35
+        }
+
+        subtitle_projects = ctk.CTkLabel(
+            self,
+            text="Projects",
+            font=ctk.CTkFont("Calibri", size=20),
+            text_color="#0f1317"
+        )
+        subtitle_projects.pack(pady=1)
+
+        button_row1 = ctk.CTkFrame(self,fg_color="transparent")
+        button_row1.pack(pady=5)
+
+        new_button = ctk.CTkButton(button_row1, text="New Project", **button_style)
+        new_button.pack(side="left", padx=10)
+
+        view_button = ctk.CTkButton(button_row1, text="View Project", **button_style)
+        view_button.pack(side="left", padx=10)
+
+        subtitle_statistics = ctk.CTkLabel(
+            self,
+            text="Statistics",
+            font=ctk.CTkFont("Calibri", size=20),
+            text_color="#0f1317"
+        )
+        subtitle_statistics.pack(pady=1)
+
+        button_row = ctk.CTkFrame(self, fg_color="transparent")
+        button_row.pack(pady=5)
+
+        reports_button = ctk.CTkButton(button_row, text="Generate report", **button_style)
+        reports_button.pack(side="left", padx=10)
+
+        resume_button = ctk.CTkButton(button_row, text="Statistics", **button_style)
+        resume_button.pack(side="left", padx=10)
+
